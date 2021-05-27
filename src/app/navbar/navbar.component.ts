c2c31379c233e1039895c863e8b6b564333d59aa
Vekaz <V4nII@hotmail.com>
2021-05-27 13:41:32 +0200

    Search generalized to use current path.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,7 +2,8 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Book, DbService } from '../db.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -13,13 +14,30 @@ export class NavbarComponent implements OnInit {
 
   books! : Observable<Book[]>;
   searchBook = this.formBuilder.group({query: ''})
+  lastUrl: string;
+  url = 'home';
+  prohibited: string[] = ["edit"];
+  
 
-  constructor(private formBuilder: FormBuilder, private dbService: DbService, private router: Router ) { 
+
+  constructor(private formBuilder: FormBuilder, private dbService: DbService, private router: Router, private route: ActivatedRoute, private location: Location ) { 
+    let urlTree = this.router.parseUrl(this.location.path());
+    this.lastUrl = this.url;
+    try {
+      this.url = urlTree.root.children.primary.segments[0].path;
+      if( this.prohibited.includes(this.url) ){
+        this.url = this.lastUrl;
+      }
+    }
+    catch (error) 
+    {
+      this.url = 'home';
+    }
   }
 
   ngOnInit(): void {}
 
   onSubmit() : void {
-    this.router.navigate(['/home/', this.searchBook.value.query]);
+    this.router.navigate([`/${this.url}/`, this.searchBook.value.query]);
   }
 }
