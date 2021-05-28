c2dcf07c7740e459318d6c0df26e646d3cbf47e5
ans99 <ans99@onet.pl>
2021-05-28 16:53:08 +0200

    Added sorting to categories list
    changed table for material table <3

--- src/app/categories-list/categories-list.component.ts
+++ src/app/categories-list/categories-list.component.ts
@@ -1,27 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
 import { Category, DbService } from '../db.service';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-categories-list',
   templateUrl: './categories-list.component.html',
-  styleUrls: ['./categories-list.component.css']
+  styleUrls: ['./categories-list.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class CategoriesListComponent implements OnInit {
+export class CategoriesListComponent implements AfterViewInit {
+  
+  displayedColumns = ["id", "name", "manage"];
+  categories!: Category[];
+  dataSource = new MatTableDataSource(this.categories);
 
-  category!: Observable<Category[]>;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private dbService: DbService, private route: ActivatedRoute) { 
-    this.category = dbService.getCategories();
+    dbService.getCategories().subscribe(categories => {
+      this.categories = categories;
+      categories.forEach(element => {
+        this.dataSource.data.push(element);
+        this.dataSource._updateChangeSubscription();
+        console.log(this.dataSource.data)
+      });
+    });
   }
 
   ngOnInit(): void {
     this.dbService.notifyObservable$.subscribe(res => {
       if(res.refresh){
-          this.category = this.dbService.getCategories();
+        this.dbService.getCategories().subscribe(categories => {
+          this.categories = categories;
+        });
       }
     });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 }
 
