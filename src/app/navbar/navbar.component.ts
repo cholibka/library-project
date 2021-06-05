82a7f1a17b45ba70c4bc87871c61fdc873229894
Vekaz <V4nII@hotmail.com>
2021-06-05 13:16:29 +0200

    Fixed getting path for search

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Book, DbService } from '../db.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 import { Location } from '@angular/common';
 
 @Component({
@@ -15,24 +15,29 @@ export class NavbarComponent implements OnInit {
   books! : Observable<Book[]>;
   searchBook = this.formBuilder.group({query: ''})
   lastUrl: string;
-  url = 'home';
+  url = '/home';
   prohibited: string[] = ['edit', 'add'];
   
 
 
   constructor(private formBuilder: FormBuilder, private dbService: DbService, private router: Router, private location: Location ) { 
-    let urlTree = this.router.parseUrl(this.location.path());
-    this.lastUrl = this.url;
-    try {
-      this.url = urlTree.root.children.primary.segments[0].path;
-      if( this.prohibited.some((val) => { return this.url.includes(val); }) ){
-        this.url = this.lastUrl;
+    this.lastUrl = this.url; // At start set base url for search as home
+    
+    this.router.events.subscribe((val) => {
+      if(val instanceof NavigationEnd){
+        try {
+          this.url = val.url;
+          if( this.prohibited.some((val) => { return this.url.includes(val); }) ){
+            this.url = this.lastUrl;
+          }
+        }
+        catch (error) 
+        {
+          this.url = '/home';
+        }
       }
-    }
-    catch (error) 
-    {
-      this.url = 'home';
-    }
+    });
+    
   }
 
   ngOnInit(): void {}
